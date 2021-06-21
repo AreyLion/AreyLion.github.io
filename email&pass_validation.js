b87da69cab2cde7a29692c1e85a9a48434c46b82
AreyLion <86210202+AreyLion@users.noreply.github.com>
2021-06-21 07:02:43 +0300

    Update email&pass_validation.js

--- email&pass_validation.js
+++ email&pass_validation.js
@@ -4,6 +4,7 @@ function validate(){
 	var email_correct = false
 	var email = document.getElementById("email");
 	var email_regexp = /[0-9a-zа-я_A-ZА-Я]+@[0-9a-zа-я_A-ZА-Я^.]+\.[a-zа-яА-ЯA-Z]{2,4}/i;
+	document.getElementById("false_email").innerHTML = "";
     // проверка значения поля email
     if (!email_regexp.test(email.value)) {
     	document.getElementById("false_email").innerHTML = "Введите корректный email.";
@@ -20,31 +21,32 @@ function validate(){
     var password_regexp2 = /[0-9]/;
     var password_regexp3 = /[a-zа-я]{1,}[A-ZА-Я]{1,}/;
     var password_regexp4 = /[!@#$%^&*]/;
+    document.getElementById("false_password").innerHTML = "";
 
     // проверка на количество знаков
     if (!password_regexp1.test(password.value)) {
-    	document.getElementById("false_password").innerHTML = "Введите не менее 6 символов";
+    	document.getElementById("false_password").innerHTML += "Введите не менее 6 символов<br />";
     }
     else {
     	password_points += 1;
     }
     // проверка на числа
     if (!password_regexp2.test(password.value)) {
-    	document.getElementById("false_password").innerHTML += "<br />Должны содержаться цифры";
+    	document.getElementById("false_password").innerHTML += "Должны содержаться цифры<br />";
     }
     else {
     	password_points += 1;
     }
     // проверка на символы нижнего регистра
     if (!password_regexp3.test(password.value)) {
-    	document.getElementById("false_password").innerHTML += "<br />Должны содержаться символы разных регистров";
+    	document.getElementById("false_password").innerHTML += "Должны содержаться символы разных регистров<br />";
     }
     else {
     	password_points += 1;
     }
     // проверка на спецсимволы
     if (!password_regexp4.test(password.value)) {
-    	document.getElementById("false_password").innerHTML += "<br />Должны содержаться спецсимволы: !@#$%^&*";
+    	document.getElementById("false_password").innerHTML += "Должны содержаться спецсимволы: !@#$%^&*";
     }
     else {
     	password_points += 1;
